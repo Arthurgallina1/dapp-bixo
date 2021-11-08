db873860e0bd2ce332bc9d71e16e46121bc59fd9
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-11-08 08:49:02 -0300

    basic erc20 token

---
+++ contracts/Cryptos.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.1 <0.9.0;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    // function allowance(address owner, address spender)
+    //     public
+    //     view
+    //     returns (uint256);
+
+    // function approve(address spender, uint256 value) public returns (bool);
+
+    // function transferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 value
+    // ) public returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract Cryptos is ERC20Interface {
+    string public name = "Cryptos";
+    string public symbol = "CRPT";
+    uint256 public decimals = 0; //18 is the most used but 0 to keep simple
+    uint256 public override totalSupply; //obrigatorio por isso override
+
+    address public founder;
+    mapping(address => uint256) public balances;
+
+    constructor() {
+        totalSupply = 1000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        override
+        returns (uint256 balance)
+    {}
+
+    function transfer(address to, uint256 tokens)
+        public
+        override
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= tokens, "not enough tokens");
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+
+        emit Transfer(msg.sender, to, tokens);
+
+        return true;
+    }
+}
